98599c8cc7151d3d7f64bd176b1a875bedce6db1
Ambrish Jeyan <ambrishjeyan023@gmail.com>
2025-12-04 16:51:22 +0530

    Updated .env

--- config/db.js
+++ config/db.js
@@ -5,17 +5,14 @@ const connectDB = async () => {
     const uri = process.env.MONGO_URI;
 
     if (!uri) {
-      throw new Error("MONGO_URI is missing from environment variables!");
+      console.log("MONGO_URI is missing from environment variables!");
+      return;
     }
 
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    await mongoose.connect(uri);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
-    console.error("MongoDB Connection Error:", error.message);
+    console.log("MongoDB Connection Error:", error);
   }
 };
 
